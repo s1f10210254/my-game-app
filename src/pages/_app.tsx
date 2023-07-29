9b15d163037a13b6353dbccc889976038d0b5bc6
s1f10210254 <iniad@DESKTOP-LC3J6L9.localdomain>
2023-07-29 18:01:10 +0900

    a

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+// index.tsx
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-export default MyApp;
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Menu from './index';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        {/* <Route path="/maze2" element={<Maze2Game />} />
+        <Route path="/minesweeper" element={<MinesweeperGame />} />
+        <Route path="/othello2" element={<Othello2Game />} /> */}
+      </Routes>
+    </Router>
+  );
+};
+export default App;
